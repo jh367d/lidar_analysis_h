aabe2029b021380a6080abfc624922a61265ad8e
jh367d <83927727+jh367d@users.noreply.github.com>
2022-10-21 14:29:44 +1100

    intial raster analysis

---
+++ code/raster_input_analysis.R
@@ -0,0 +1,10 @@
+library(raster)
+r = raster("Map2.tif")
+plot(r)
+r
+hist(r[], main=NULL)
+library(tmap)
+tm_shape(r) + tm_raster(style= "sd", n = 10 , palette = "Greys")
+tm_shape(r) + tm_raster(style= "cont", palette = "Greys")  + tm_legend(outside = TRUE)
+proj4string(r)
+
